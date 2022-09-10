4bd6afdc1050651ea1a9de698fcf8aa1c8f0dce2
dansusman <frenchminister@gmail.com>
2022-09-10 17:05:46 -0400

    Add TODO highlighting

--- nvim/.config/nvim/lua/susmand/packer.lua
+++ nvim/.config/nvim/lua/susmand/packer.lua
@@ -25,6 +25,18 @@ return require("packer").startup(function()
 
     use("mbbill/undotree")
 
+    use {
+      "folke/todo-comments.nvim",
+      requires = "nvim-lua/plenary.nvim",
+      config = function()
+        require("todo-comments").setup {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        }
+      end
+    }
+
     -- Colorscheme section
     use("gruvbox-community/gruvbox")
     use("folke/tokyonight.nvim")
