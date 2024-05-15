b6cc3312e919d3d5c70b27a6cd1b570a3a3cc4f9
Dan Susman <frenchminister@gmail.com>
2024-05-15 17:45:56 -0400

    Add harpoon settings to init.lua directly

--- nvim/init.lua
+++ nvim/init.lua
@@ -155,6 +155,42 @@ require("lazy").setup({
 	},
 	{
 		"ThePrimeagen/harpoon",
+		config = function()
+			local nmap = require("ds.keymap").nmap
+
+			local harpoon = require("harpoon")
+
+			harpoon.setup({})
+
+			nmap({ "<leader>a", require("harpoon.mark").add_file })
+			nmap({ "<C-e>", require("harpoon.ui").toggle_quick_menu })
+			nmap({ "<leader>tc", require("harpoon.cmd-ui").toggle_quick_menu })
+
+			nmap({
+				"<C-h>",
+				function()
+					require("harpoon.ui").nav_file(1)
+				end,
+			})
+			nmap({
+				"<C-j>",
+				function()
+					require("harpoon.ui").nav_file(2)
+				end,
+			})
+			nmap({
+				"<C-k>",
+				function()
+					require("harpoon.ui").nav_file(3)
+				end,
+			})
+			nmap({
+				"<C-l>",
+				function()
+					require("harpoon.ui").nav_file(4)
+				end,
+			})
+		end,
 	},
 	-- NOTE: Plugins can also be added by using a table,
 	-- with the first argument being the link and the following
