b872555e1a02e3980b0f48e8cd38cf3d71e18bb8
Dan Susman <frenchminister@gmail.com>
2024-07-05 15:48:20 -0400

    [sketchy]: Update selected workspace UI

--- config/.config/sketchybar/plugins/space.sh
+++ config/.config/sketchybar/plugins/space.sh
@@ -4,4 +4,8 @@
 # the space invoking this script (with name: $NAME) is currently selected:
 # https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
 
-sketchybar --set "$NAME" background.drawing="$SELECTED"
+if [ "$SELECTED" = "true" ]; then
+  sketchybar -m --set $NAME icon.highlight=on background.drawing=on background.border_width=1
+else
+  sketchybar -m --set $NAME icon.highlight=off background.drawing=off background.border_width=0
+fi
