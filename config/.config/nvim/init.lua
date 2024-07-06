cd4052dca680ca403242c052473ed2c82f82dc9d
Dan Susman <frenchminister@gmail.com>
2024-07-06 15:42:58 -0400

    [nvim]: Add oil.nvim

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -145,6 +145,23 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+	{
+		'stevearc/oil.nvim',
+		opts = {},
+		config = function()
+			require("oil").setup({
+				default_file_explore = true,
+				view_options = {
+					show_hidden = true,
+				},
+			})
+			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+			vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+		end,
+		-- Optional dependencies
+		dependencies = { "echasnovski/mini.icons" },
+		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+	},
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 	{
 		"blazkowolf/gruber-darker.nvim",
