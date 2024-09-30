eacf995535daf38009b827c029cd4332257c9eb0
Dan Susman <frenchminister@gmail.com>
2024-09-30 12:19:31 -0400

    [nvim]: Add duplicate-line remap (inspired by Emacs)

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -108,6 +108,7 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<C-y>", "mmyyp`mj", { desc = "Duplicate line and keep cursor position" })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
