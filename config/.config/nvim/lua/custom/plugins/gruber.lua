7359f0f3f25556fce056be222509cef04eaa7c71
Dan Susman <frenchminister@gmail.com>
2026-04-30 09:08:35 -0400

    feat(nvim): update to light theme
    Never thought I'd be doing this to be honest... but here we are.

--- config/.config/nvim/lua/custom/plugins/gruber.lua
+++ config/.config/nvim/lua/custom/plugins/gruber.lua
@@ -3,7 +3,14 @@ return {
     "dansusman/gruber-darker.nvim",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("gruber-darker")
+      -- vim.cmd.colorscheme("gruber-darker")
+    end,
+  },
+  {
+    "dansusman/gruber-lighter.nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("gruber-lighter")
     end,
   },
 }
